2c30d9a0e4907b2bd645620928c5e00146e41155
ysrastogi <ysrastogi19@gmail.com>
2025-10-16 05:43:44 +0530

    feat: Add Qdrant URL configuration to settings

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     deriv_auth_token: str = os.environ.get("DERIV_AUTH_TOKEN", "")
     gemini_api_key: str = os.environ.get("GEMINI_API_KEY", "")
     qdrant_api_key: Optional[str] = os.environ.get("QDRANT_API_KEY", None)
+    qdrant_url: Optional[str] = os.environ.get("QDRANT_URL", "http://localhost:6333")
     api_host: str = os.environ.get("API_HOST", "")
     api_port: int = int(os.environ.get("API_PORT", 9000))
     ws_host: str = os.environ.get("WS_HOST", "")
